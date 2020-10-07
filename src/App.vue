8720027a12a6c4a793b56e7f6a388717316164aa
Chris Lewis <chris@chrislewis.codes>
2020-10-07 10:29:19 -0700

    Add RM logo

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <style lang="scss">
+  #readability-matters-site {
+    width: 100%;
+    > header {
+      border-bottom: 2px solid #d8c046;
+      #logo-link {
+        $height: 1in;
+        display: inline-block;
+        height: $height;
+        $padding: $height * 11 / 96;
+        padding-left: calc(4vw - #{$padding});
+        img {
+          height: 100%;
+          width: auto;
+        }
+      }
+    }
+  }
 </style>
 
 <template>
   <div id="readability-matters-site">
+    <header>
+      <a id="logo-link" href="https://readabilitymatters.org/">
+        <img src="./assets/images/rm-logo.png">
+      </a>
+    </header>
     <Sandbox />
   </div>
 </template>
